d359cf06369df069a5e8c7fb8d8b84c7b405b304
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-04-16 16:25:28 +0700

    update

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -4,12 +4,13 @@ export default function WelcomeMessage() {
   return (
     <div className="welcome-message__container">
       <h1 className="welcome-message__topic">What you will read today?</h1>
-      <h3 className="welcome-message__tip">
-        Get Start : <br />
-        1. Search your first book.
-        <br /> 2. Add the book to your "Shelf".
-        <br /> 3. Add the book you wanna read to "Bag"
-      </h3>
+
+      <div className="welcome-message__tip-wrapper">
+        <h4>Get Start :</h4>
+        <p>1. Search your first book.</p>
+        <p>2. Add the book to your "Shelf".</p>
+        <p>3. Add the book you wanna read to "Bag"</p>
+      </div>
     </div>
   )
 }
